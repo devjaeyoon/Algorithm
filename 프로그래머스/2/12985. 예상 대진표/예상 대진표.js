2f5518e47b6950b0ee8b5246f15fcb9a3ac1fe99
devjaeyoon <devjaeyoon@gmail.com>
2024-04-07 01:05:07 +0900

    [level 2] Title: 예상 대진표, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,14 +1,13 @@
-function solution(n,a,b)
-{
-    let currentA = a;
-    let currentB = b;
-    let answer = 1;
-    
-    while (Math.floor((currentA + 1) / 2) !== Math.floor((currentB + 1) / 2)) {
-           currentA = Math.floor((currentA + 1) / 2)
-           currentB = Math.floor((currentB + 1) / 2)
+function solution(n, a, b) {
+  let currentA = a;
+  let currentB = b;
+  let answer = 1;
+
+  while (Math.floor((currentA + 1) / 2) !== Math.floor((currentB + 1) / 2)) {
+    currentA = Math.floor((currentA + 1) / 2);
+    currentB = Math.floor((currentB + 1) / 2);
     answer++;
-           }
-    
-    return answer;
+  }
+
+  return answer;
 }
